d83fdea87a815dd998d4e97c0f4687a9728ca867
Michael Douglas B. Araujo <michaeldouglas010790@gmail.com>
2023-10-06 22:05:14 -0300

    Meu primeiro projeto

---
+++ src/javascript/1.js
@@ -0,0 +1,21 @@
+// Iniciando o ponteiro de memoria
+//string nome;
+//var nome;
+//nome = 'Ola, mundo!';
+//console.log('TESTE');
+
+// Pergunta de entrevista?
+var num = 0;
+num = num + 2;
+num = num * 3;
+num = num / 2;
+num++;
+num--;
+num += 1;
+num -= 2;
+num *= 3;
+num /= 2;
+
+var divElement = document.getElementById("resultado");
+
+divElement.innerHTML = num;
