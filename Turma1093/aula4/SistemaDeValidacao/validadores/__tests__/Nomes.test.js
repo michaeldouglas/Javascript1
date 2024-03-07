8acc61234e0751d8428fc7401fbc54fe1d3984c5
Michael Douglas Barbosa Araujo <michaeldouglas010790@gmail.com>
2024-03-06 21:15:55 -0300

    Ajuste do nome

--- Turma1093/aula4/SistemaDeValidacao/validadores/__tests__/Nomes.test.js
+++ Turma1093/aula4/SistemaDeValidacao/validadores/__tests__/Nomes.test.js
@@ -3,8 +3,12 @@ const Nomes = require("../Nomes");
 describe("[Suite_Testes_1093_Projeto][Nomes] Suíte de testes módulo (nome)", () => {
   // Testes da versão 1
   describe("[VERSAO_1] Regra de negócios v1", () => {
-    test("Deve retornar true para um nome válido por exemplo: João Silva", () => {
+    test("Deve retornar false para um nome que não está válido por exemplo: João Silva", () => {
       expect(Nomes("João Silva")).toBe(false);
+    });
+
+    test("Deve retornar false para um nome vazio por exemplo: '' ", () => {
+      expect(Nomes("")).toBe(false);
     });
   });
 });
