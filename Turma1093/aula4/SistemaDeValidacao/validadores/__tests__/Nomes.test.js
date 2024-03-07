20f54044990ef67a6eec9e735dd6680c13c07929
Michael Douglas Barbosa Araujo <michaeldouglas010790@gmail.com>
2024-03-06 21:34:17 -0300

    Ajuste do teste de nome

--- Turma1093/aula4/SistemaDeValidacao/validadores/__tests__/Nomes.test.js
+++ Turma1093/aula4/SistemaDeValidacao/validadores/__tests__/Nomes.test.js
@@ -1,6 +1,25 @@
 const Nomes = require("../Nomes");
 
 describe("[Suite_Testes_1093_Projeto][Nomes] Suíte de testes módulo (nome)", () => {
+  // Testes do default
+  describe("[DEFAULT] Regra de negócios default", () => {
+    test("Deve retornar true para um nome válido por exemplo: Maria Sousa", () => {
+      expect(Nomes("Maria Sousa", "default")).toBe(true);
+    });
+
+    test("Deve retornar false para um nome vazio por exemplo: ''", () => {
+      expect(Nomes("", "default")).toBe(false);
+    });
+
+    test("Devo retornar true para um nome com apenas espaços em branco '    ' ", () => {
+      expect(Nomes("    ", "default")).toBe(true);
+    });
+
+    test("Deve retornar true para um nome com caracteres especiais por exemplo: Carlos Oliviera@filho.com.br", () => {
+      expect(Nomes("Carlos Oliviera@filho.com.br", "default")).toBe(true);
+    });
+  });
+
   // Testes da versão 1
   describe("[VERSAO_1] Regra de negócios v1", () => {
     test("Deve retornar false para um nome que não está válido por exemplo: João Silva", () => {
