4d3404408e5a6c28d1be243a8dd48562b512866c
Michael Douglas Barbosa Araujo <michaeldouglas010790@gmail.com>
2024-03-08 22:05:47 -0300

    Ajuste dos testes e documentação do trabalho

--- Turma1093/aula4/SistemaDeValidacao/validadores/__tests__/Estados.jest.js
+++ Turma1093/aula4/SistemaDeValidacao/validadores/__tests__/Estados.jest.js
@@ -2,7 +2,53 @@ const Estados = require("../Estados");
 
 describe("[Suite_Testes_1093_Projeto][Estados] Suíte de testes módulo (estados)", () => {
   test("Deve retornar true para um estado válido exemplo: São Paulo", () => {
+    //@TODO: Objeto e repetição
     const estado = new Estados("São Paulo"); // SP // SP São Paulo // SP - São Paulo
     expect(estado.valido).toBe(true);
+  });
+
+  test("Deve retornar false para uma string vazio '' ", () => {
+    const estado = new Estados("");
+    expect(estado.valido).toBe(false);
+  });
+
+  test("Deve retornar false para um espaço em branco ' ' ", () => {
+    const estado = new Estados(" ");
+    expect(estado.valido).toBe(false);
+  });
+
+  test("Deve retornar true para um estado inexistes por exemplo: SÃO PEDRO", () => {
+    const estado = new Estados("SÃO PEDRO");
+    expect(estado.valido).toBe(true);
+  });
+
+  test("Deve retornar false para um número de estado por exemplo 12:", () => {
+    const estado = new Estados(12);
+    expect(estado.valido).toBe(false);
+  });
+
+  test("Deve retornar true em casos onde o estado possui caracteres especiais por exemplo: São Paulo !@#$%&%#%$%", () => {
+    const estado = new Estados("São Paulo !@#$%&%#%$%");
+    expect(estado.valido).toBe(true);
+  });
+
+  test("Deve retornar true para um estado que contenha números junto ao estado: São Paulo - 2024", () => {
+    const estado = new Estados("São Paulo - 2024");
+    expect(estado.valido).toBe(true);
+  });
+
+  test("Deve retornar true para emojis por exemplo: São Paulo 😀😐😐😐😐😐😐😐", () => {
+    const estado = new Estados("São Paulo 😀 😐😐😐😐😐😐😐");
+    expect(estado.valido).toBe(true);
+  });
+
+  test("Deve retornar true para um estado com traços e espaços por exemplo: Minas Gerais - MG ", () => {
+    const estado = new Estados("Minas Gerais - MG ");
+    expect(estado.valido).toBe(true);
+  });
+
+  test("Deve retornar false para um estado com apenas um espaço '' ", () => {
+    const estado = new Estados(" ");
+    expect(estado.valido).toBe(false);
   });
 });
---
+++ Turma1093/trabalho.md
@@ -0,0 +1,36 @@
+# Trabalho estrutura do projeto
+
+- Criar o repositório
+- Algum membro da Squad vai criar, então criem em grupo porque o responsável terá que
+- compartilhar o repo com os outros
+- Team members
+- Tasks - criar as tasks do projeto
+- Todas as tarefas devem estar em "done" e todos os membros da equipe precisam fazer algo,
+- mesmo que seja apenas um comentário em um readme.md
+- Branch separada sim, seguindo a regra: feature para dev e release para prod,
+- com comentários.
+
+O nosso projeto deverá ser divido em 3 partes:
+
+- front
+- api
+- bff
+
+Front do projeto é uma cálculadora de dividendos super simples pode escolher ação ou o que vocês acharem melhor....
+
+### Qual é foco
+
+Não perder muito tempo em front super bonito se for um html com tela branca e botão zinza tá ok!!! -> A não ser que queiram fazer
+
+Deve chamar a API super simples com express, não precisa autenticação
+não precisa de super patterns e afins -> A não ser que queiram fazer
+Não precisa fazer o cálculo estilo B3 -> 20 passou 1 mês ? Quanto rendeu??????? Pode ser exemplo 50 passou 1 mês você ganhou 0.3 somar e devolver
+
+back [BFF] -> para a jornada Ele deve chamar a API onde terá talvez 1 rota vai depender de vocês /calcular-dividendos e devolver o caso mencionando antes 50 passou 1 mês quanto rendeu?????
+
+Outra questão e se eu passar menos de 1 mês não deveria dizer o dividendo!!!!!
+
+### A API E O BFF deve ter teste!
+
+Os teste cada squad vai ter 1 e a filosofia no dia batemos um papo!
+O GitHub Actions executando o teste é interessante !!!
